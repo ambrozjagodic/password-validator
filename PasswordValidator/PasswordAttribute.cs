494f8d0bfc3503a835009b20a15ed4f8b676cc4a
Ambrož Jagodic <ambroz.jagodic@gmail.com>
2023-08-14 09:50:51 +0200

    password validator implemented

---
+++ PasswordValidator/PasswordAttribute.cs
@@ -0,0 +1,210 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Validates the format and complexity of a password string.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class PasswordAttribute : ValidationAttribute
+{
+    private int _minLength = 12;
+    private int _maxLength = 64;
+    private int _requiredCharacterTypes = 3;
+
+    /// <summary>
+    /// Gets or sets the minimum allowed password length. Default is 12.
+    /// </summary>
+    public int MinLength
+    {
+        get => _minLength;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{nameof(MinLength)} must be greater than 0.");
+            }
+            _minLength = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the maximum allowed password length. Default is 64.
+    /// </summary>
+    public int MaxLength
+    {
+        get => _maxLength;
+        set
+        {
+            if (value < MinLength)
+            {
+                throw new ArgumentException($"{nameof(MaxLength)} must be greater than or equal to MinLength.");
+            }
+            _maxLength = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the number of required distinct character types in the password (uppercase letters, lowercase letters, numbers, special characters). Default is 3.
+    /// </summary>
+    public int RequiredCharacterTypes
+    {
+        get => _requiredCharacterTypes;
+        set
+        {
+            if (value < 0 || value > 4)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(RequiredCharacterTypes)} must be between 0 and 4.");
+            }
+
+            _requiredCharacterTypes = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets whether uppercase letters must be enforced in the password.
+    /// </summary>
+    public bool EnforceUppercase { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets whether lowercase letters must be enforced in the password.
+    /// </summary>
+    public bool EnforceLowercase { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets whether numbers must be enforced in the password.
+    /// </summary>
+    public bool EnforceNumbers { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets whether special characters must be enforced in the password.
+    /// </summary>
+    public bool EnforceSpecialChars { get; set; } = false;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PasswordAttribute"/> class.
+    /// </summary>
+    public PasswordAttribute()
+    {
+    }
+
+    /// <inheritdoc />
+    public override bool IsValid(object? value)
+    {
+        if (value == null || value is not string password)
+        {
+            return false;
+        }
+
+        // Check password length
+        if (!IsValidLength(password, out var lengthErrorMessage))
+        {
+            ErrorMessage = lengthErrorMessage;
+            return false;
+        }
+
+        var overrideRequiredCharTypeValidation = EnforceUppercase || EnforceLowercase || EnforceNumbers || EnforceSpecialChars;
+        if (overrideRequiredCharTypeValidation)
+        {
+            // Check specific character requirements based on properties
+            if (!IsValidByEnforceProps(password, out var enforcePropsErrorMessage))
+            {
+                ErrorMessage = enforcePropsErrorMessage;
+                return false;
+            }
+        }
+        else
+        {
+            // Check required character types if no Enforce properties are set
+            if (!IsValidByCharTypes(password, out var charTypesErrorMessage))
+            {
+                ErrorMessage = charTypesErrorMessage;
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+
+
+    private bool IsValidLength(string password, out string errorMessage)
+    {
+        errorMessage = string.Empty;
+
+        if (password.Length < MinLength || password.Length > MaxLength)
+        {
+            errorMessage = $"Password length must be between {MinLength} and {MaxLength} characters.";
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool IsValidByEnforceProps(string password, out string errorMessage)
+    {
+        errorMessage = string.Empty;
+        if (EnforceUppercase && !password.Any(char.IsUpper))
+        {
+            errorMessage = "Password must include at least one uppercase letter.";
+            return false;
+        }
+
+        if (EnforceLowercase && !password.Any(char.IsLower))
+        {
+            errorMessage = "Password must include at least one lowercase letter.";
+            return false;
+        }
+
+        if (EnforceNumbers && !password.Any(char.IsDigit))
+        {
+            errorMessage = "Password must include at least one number.";
+            return false;
+        }
+
+        if (EnforceSpecialChars && !password.Any(c => char.IsSymbol(c) || char.IsPunctuation(c)))
+        {
+            errorMessage = "Password must include at least one special character.";
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool IsValidByCharTypes(string password, out string errorMessage)
+    {
+        errorMessage = string.Empty;
+
+        int actualCharacterTypes = 0;
+
+        if (password.Any(char.IsUpper))
+        {
+            actualCharacterTypes++;
+        }
+
+        if (password.Any(char.IsLower))
+        {
+            actualCharacterTypes++;
+        }
+
+        if (password.Any(char.IsDigit))
+        {
+            actualCharacterTypes++;
+        }
+
+        if (password.Any(c => char.IsSymbol(c) || char.IsPunctuation(c)))
+        {
+            actualCharacterTypes++;
+        }
+
+        if (!EnforceUppercase && !EnforceLowercase && !EnforceNumbers && !EnforceSpecialChars)
+        {
+            if (actualCharacterTypes < RequiredCharacterTypes)
+            {
+                errorMessage = $"Password must include at least {RequiredCharacterTypes} of the following character types: uppercase letters, lowercase letters, digits, special symbols.";
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,114 @@
+# PasswordValidator
+
+The **PasswordAttribute** is a .NET validation attribute that allows you to validate the format and complexity of password strings. It provides customizable rules to enforce password requirements such as length, character types, and more.
+
+## Installation
+
+To use the PasswordAttribute in your .NET Core project, you can install it via NuGet Package Manager:
+
+```sh
+dotnet add package DoesntExistYet
+```
+
+## Usage
+
+1. Add the `PasswordAttribute` to your Password property in your desired class:
+
+```csharp
+using System.ComponentModel.DataAnnotations;
+
+public class UserModel
+{
+    public string Name { get; set; }
+
+    [Password]
+    public string Password { get; set; }
+}
+```
+
+2. Apply the model validation in your code, such as during registration or password update:
+
+```csharp
+var userModel = new UserModel
+{
+    Name = "JohnDoe",
+    Password = "P@ssw0rd123"
+};
+
+var validationContext = new ValidationContext(userModel);
+var validationResults = new List<ValidationResult>();
+
+bool isValid = Validator.TryValidateObject(userModel, validationContext, validationResults, validateAllProperties: true);
+
+if (!isValid)
+{
+    foreach (var validationResult in validationResults)
+    {
+        Console.WriteLine(validationResult.ErrorMessage);
+    }
+}
+```
+
+> **Note:** The example provided shows a manual validation process. If you're using a framework like ASP.NET Core, model validation is often handled automatically as part of the request processing.
+
+## Customization
+
+The `PasswordAttribute` provides several properties that allow you to customize the password validation rules according to your requirements. Here is an overview of each property, its default value, and an explanation of its purpose:
+
+- **MinLength (int, default: 12)**
+
+Specifies the minimum allowed length for the password. Must be greater than 0.
+
+- **MaxLength (int, default: 64)**
+
+Specifies the maximum allowed length for the password. The maximum length must be greater than or equal to the `MinLength`.
+
+- **RequiredCharacterTypes (int, default: 3)**
+
+Specifies the number of required distinct character types in the password. Character types include:
+
+- Uppercase letters
+- Lowercase letters
+- Numbers
+- Special characters
+
+This property ensures that the password contains a combination of the specified character types. So by default, 3 of the 4 character types must be included in a password.
+
+- **EnforceUppercase (bool, default: false)**
+
+Indicates whether uppercase letters must be enforced in the password. If set to `true`, the password must contain at least one uppercase letter.
+
+- **EnforceLowercase (bool, default: false)**
+
+Indicates whether lowercase letters must be enforced in the password. If set to `true`, the password must contain at least one lowercase letter.
+
+- **EnforceNumbers (bool, default: false)**
+
+Indicates whether numbers must be enforced in the password. If set to `true`, the password must contain at least one number.
+
+- **EnforceSpecialChars (bool, default: false)**
+
+Indicates whether special characters must be enforced in the password. If set to `true`, the password must contain at least one special character (symbol or punctuation).
+
+> **Note:** If any of the above `Enforce` properties are set, the default `RequiredCharacterTypes` validation is bypassed.
+
+### Example Usage with Cusomization
+
+Here's an example of how to use the `PasswordAttribute` with customized validation rules:
+
+```csharp
+using System.ComponentModel.DataAnnotations;
+
+public class UserModel
+{
+    [Password(MinLength = 8, MaxLength = 20, EnforceUppercase = true, EnforceNumbers = true)]
+    public string Password { get; set; }
+}
+```
+
+## Contributions
+
+Contributions are welcome! If you encounter any issues or have suggestions for improvements, please feel free to open an issue or submit a pull request.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/ambrozjagodic/PasswordValidator/blob/dcd2c8bd114309682e792aa9d40f693a133bd2f7/LICENSE) file for details.
